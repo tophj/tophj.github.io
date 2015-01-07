612ba13b9579fa2d42358532cc39b850ed2d739a
tophj <tophj@vt.edu>
2015-01-06 23:10:55 -0500

    Updated graph

--- graph.js
+++ graph.js
@@ -8,28 +8,11 @@ function renderGraph(){
 	var h = 350;
 	var padding = 40;
 
-	var dataset = [];					//Initialize empty array
-	var numDataPoints = 50;				//Number of dummy data points to create
-	var xRange = 30;	//Max range of new x values
-	var yRange = 3000;	//Max range of new y values
-
-	//Dynamic, random dataset
-	// for (var i = 0; i < numDataPoints; i++) {					//Loop numDataPoints times
-	// 	var newNumber1 = Math.round(Math.random() * xRange);	//New random integer
-	// 	var newNumber2 = Math.round(Math.random() * yRange);	//New random integer
-	// 	dataset.push([newNumber1, newNumber2]);					//Add new number to array
-	// }
-
-	//Create averages for days
-	for (var i = 0; i < jsonPings.pings.length; i++){
-		//get the date and convert it to a 
-		var x = jsonPings.pings[i].date;
-
-	}
+	var parseDate = d3.time.format("%H:%M").parse;
 
 	//Create scale functions
 	var xScale = d3.time.scale()
-						 
+						 .range([padding, w - padding * 2]);
 
 	var yScale = d3.scale.linear()
 						 .domain([0, 3000])
@@ -43,7 +26,7 @@ function renderGraph(){
 	var xAxis = d3.svg.axis()
 					  .scale(xScale)
 					  .orient("bottom")
-					  .ticks(5);
+				.tickFormat(d3.time.format("%H:%M"));
 
 	//Define Y axis
 	var yAxis = d3.svg.axis()
@@ -57,23 +40,25 @@ function renderGraph(){
 				.attr("width", w)
 				.attr("height", h);
 
+	(jsonPings.pings).forEach(function(d){
+		d.time = parseDate(d.time);
+	});
 
 
-
+	xScale.domain(d3.extent(jsonPings.pings, function(d) { return d.time; }));
+    
 	//Create circles
 	svg.selectAll("circle")
-	   .data(dataset)
+	   .data(jsonPings.pings)
 	   .enter()
 	   .append("circle")
 	   .attr("cx", function(d) {
-	   		return xScale(d[0]);
+	   		return xScale(d.time);
 	   })
 	   .attr("cy", function(d) {
-	   		return yScale(d[1]);
+	   		return yScale(d.ping);
 	   })
-	   .attr("r", function(d) {
-	   		return rScale(d[1]);
-	   })
+	   .attr("r", 1)
 	   .attr("fill", "white");
 
 	/*
